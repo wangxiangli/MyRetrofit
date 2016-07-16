14554624931e3965578acdcce381372bc2fbec2c
3102449184 <3102449184@qq.com>
2016-07-16 10:55:18 +0800

    post

--- myretrofit/src/main/java/com/bush/myretrofit/RetrofitService.java
+++ myretrofit/src/main/java/com/bush/myretrofit/RetrofitService.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.QueryMap;
 
@@ -18,4 +19,12 @@ public interface RetrofitService {
 
     @GET("v1/weather/query?province=江苏&key=14b49edf0cfb1&city=南京")
    Call<ResponseBody> getResult();
+
+
+    @POST("{path}?")
+    Call<ResponseBody> getPost(@Path("path")
+                               String path,
+                               @QueryMap
+                               HashMap<String ,String> map);
+
 }
